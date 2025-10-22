0fc47e97e95316433f4ed6fe905f8e862dc5bf9c
KILLUA HUB <xusx238@gmail.com>
2025-10-22 09:55:07 +0700

    Update Firebase configuration with new credentials

--- firebase-config.js
+++ firebase-config.js
@@ -4,14 +4,14 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase
 import { getDatabase } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBh6lHioC_10xwsNDIofFhk2wcTlp8cJyA",                 // your api key
-  authDomain: "key3-9659b.firebasestorage.app",         // your auth domain e.g. your-app.firebaseapp.com
-  databaseURL: "https://key3-9659b-default-rtdb.firebaseio.com/",      // e.g. https://your-app-default-rtdb.firebaseio.com
-  projectId: "key3-9659b",           // your firebase project id
-  storageBucket: "key3-9659b.firebasestorage.app",   // e.g. your-app.appspot.com
+apiKey: "AIzaSyB_Lu37H2-Hr7bkmNWXAYE6VXtrjpfT6SI",
+  authDomain: "key3-9659b.firebaseapp.com",
+  databaseURL: "https://key3-9659b-default-rtdb.firebaseio.com",
+  projectId: "key3-9659b",
+  storageBucket: "key3-9659b.firebasestorage.app",
   messagingSenderId: "27875640853",
-  appId: "1:27875640853:android:1cf7f7b41e8c292d21301c",
-  measurementId: "com.controlPainel12"
+  appId: "1:27875640853:web:3819bef9d411a2d721301c",
+  measurementId: "G-EYRMKS5F02"
 };
 
 // Initialize Firebase
